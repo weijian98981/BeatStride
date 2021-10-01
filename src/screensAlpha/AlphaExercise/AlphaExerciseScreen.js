4a52bd873fba21023e6d5abec29a61e36e49cd36
BarnabasLim <blim048@e.ntu.edu.sg>
2021-10-01 21:56:55 +0800

    Added screensAlpha in src

---
+++ src/screensAlpha/AlphaExercise/AlphaExerciseScreen.js
@@ -0,0 +1,84 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { StyleSheet,  Text,  View, Dimensions, Animated, TouchableWithoutFeedback } from 'react-native';
+import { auth as SpotifyAuth,  remote as SpotifyRemote } from 'react-native-spotify-remote';
+
+import Screen from '../MainScreen';
+
+//import components
+
+
+const {width, height} = Dimensions.get("window")
+
+
+/**
+ * This is a functional component representing the Exercise screen.
+ * 
+ * @author NTU CZ2006 Team Alpha
+ */
+const AlphaExerciseScreen = () => {
+
+
+    return (
+        <Screen title={"Exercise"}>
+            {/* Header */}
+            <View style={styles.header}>
+                <Text style={styles.headerText}>Exercise </Text>
+                
+            </View>
+            <View>
+                <Text>Play Space Omkar and Barnabas</Text>
+            </View>
+        </Screen>
+    );
+};
+
+const styles = StyleSheet.create({
+    header:{
+        width: width,
+        height: height * 0.1,
+        justifyContent:'center',
+        paddingHorizontal: '10%',
+        backgroundColor: '#1e2124',
+    },
+    headerText:{
+        color: '#BABBBF',
+        fontSize: 28,
+        fontWeight: 'bold',
+        height: height * 0.1,
+        includeFontPadding: false,
+        textAlignVertical: 'center',
+    },
+    tabIndicator:{
+        width: width,
+        height: height * 0.07,
+        backgroundColor: '#1e2124',
+        overflow: 'hidden',
+    },
+    tab:{
+        width: width * 0.5,
+        height: height * 0.07,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+    },
+    tabText:{
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: '#BABBBF',
+    },
+    tabHighlight:{
+        width: height * 0.02,
+        height: height * 0.02,
+        borderRadius: height,
+        position: 'absolute',
+        alignSelf: 'center',
+        transform: [{translateY: -(height * 0.01) }]
+    },
+    scrollview:{
+        // backgroundColor: 'green',
+        height: height * 0.73,
+    },
+})
+
+export default AlphaExerciseScreen;
---
+++ src/screensAlpha/MainScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {  SafeAreaView,  StyleSheet,  Text,  View, Dimensions } from 'react-native';
+
+const {width, height} = Dimensions.get("window")
+
+/**
+ * This is a functional component representing the main styling screen throughout the application.
+ * 
+ * @author NTU CZ2006 Team Alpha
+ */
+ const Screen = (props) => {
+    return (
+      <SafeAreaView style={styles.screen}>
+  
+          <View>
+              {props.children}
+          </View>
+  
+          <View style={styles.navigationBar}/>
+          
+      </SafeAreaView>
+    );
+  };
+
+const styles = StyleSheet.create({
+    screen:{
+        width: width,
+        height: height,
+        backgroundColor: '#282b30',
+    },
+    navigationBar:{
+        width: width,
+        height: height * 0.1,
+        backgroundColor: '#18191D',
+        bottom: 0,
+        position: 'absolute',
+        // borderBottomWidth: height * 0.05,
+        // borderBottomColor: 'yellow',
+    },
+})
+
+export default Screen;
